deddec70add8061b691644337b1e6d1a03c4a5bf
Anirudha Anekal <77496170+Wambyat@users.noreply.github.com>
2022-06-03 14:51:17 +0530

    Update Client3.go

--- Sending and recieveing JSON/Client3.go
+++ Sending and recieveing JSON/Client3.go
@@ -13,21 +13,30 @@ func main() {
 	httpurl := "https://reqres.in/api/users"
 	fmt.Println("API URL: ", httpurl)
 
+	//We format the jsonData into byte so it can be transmitted
 	var jsonData = []byte(`{
 		"name": "morpheus",
 		"job": "leader"
 	}`)
 
+	//Here we POST the JSON
 	request, error := http.NewRequest("POST", httpurl, bytes.NewBuffer(jsonData))
 	request.Header.Set("Content-Type", "application/json; charset=UTF-8")
 
 	client := &http.Client{}
 	response, error := client.Do(request)
+	
 	if error != nil {
+		
+		//"Nicer" way to deal with the error
 		panic(error)
 	}
+	
 	defer response.Body.Close()
+	
 	body, _ := ioutil.ReadAll(response.Body)
+	//Here we can ignore error because we have used panic()
+	
 	fmt.Println("response Body:", string(body))
 
 }
