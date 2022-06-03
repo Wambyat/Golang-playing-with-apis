884b19c2a5b5f2e2b54b0bdaf29c5e73dcc49db5
Anirudha Anekal <77496170+Wambyat@users.noreply.github.com>
2022-06-03 14:47:37 +0530

    Update client.go

--- Talking overlocalhost/client.go
+++ Talking overlocalhost/client.go
@@ -7,15 +7,22 @@ import (
 )
 
 func main() {
-	fmt.Println("uwu starting ")
+	fmt.Println(" starting ")
+	//Message is stored after "http://localhost:8080/
 	resp, err := http.Get("http://localhost:8080/noice")
+	
+	//If err occurs print that
 	if err != nil {
 		fmt.Println(err)
 	}
+	
 	body, err := ioutil.ReadAll(resp.Body)
+	
+	//If err occurs print that	
 	if err != nil {
 		fmt.Println(err)
 	}
+	
 	sb := string(body)
 	fmt.Println(sb)
 }
